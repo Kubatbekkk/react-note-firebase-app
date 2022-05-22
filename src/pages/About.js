aad1b39dc9204f838bc3b179cdd61691e332188f
Kubatbekkk <kubineccastro@gmail.com>
2022-05-23 00:44:04 +0600

    Initial

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const About = () => {
+  return (
+    <div className="container-fluid py-5">
+      <h1 className="display-5 fw-bold">Лучшее React Приложение</h1>
+      <p className="col-md-8 fs-4">
+        Версия приложения <span>1.0.42</span>
+      </p>
+    </div>
+  )
+}
